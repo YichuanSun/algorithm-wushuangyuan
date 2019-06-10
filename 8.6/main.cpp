c04a92354443ecbe031f77d2e1f3a6599fe22a84
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-06-10 09:39:05 +0800

    bug了

--- 8.6/main.cpp
+++ 8.6/main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int a[N][N],flag,n,m;
+pair<int,int> vm;
+stack<int> si,ans;
+
+void dfs(int x,int y);
+bool mov(int x,int y,int op);
 
 int main()  {
     cin>>n>>m;
@@ -10,33 +15,71 @@ int main()  {
         for (int j=0;j<m;j++)
             cin>>a[i][j];
     dfs(0,0);
+    while (!si.empty()) {ans.push(si.top());si.pop();}
+    while (!ans.empty())    {
+        cout<<ans.top()<<' ';
+        ans.pop();
+    }
+    cout<<endl;
     return 0;
 }
 
 void dfs(int x,int y)   {
+    cout<<x<<' '<<y<<endl;
     if (flag)   {return;}
-    mov(x,y,op);    //检验是否可以这样移动
+    if (x==n-1&&y==m-1) {flag=1;return;}
+    //mov(x,y,op);    //检验是否可以这样移动
     if (x<0||x>=n||y<0||y>=m)   {return;}
     if (a[x][y]==1) {return;}
-    for (int i=1;i<=8;i++)  {
+    for (int i=8;i>=1;i--)  {
         if (i==1||i==3||i==5)   continue;
         if (mov(x,y,i)) {
-
+            si.push(i);
+            x+=vm.first;
+            y+=vm.second;
+            dfs(x,y);
+            if (flag)   return;
+            x-=vm.first;
+            y-=vm.second;
         }
     }
 }
 
 bool mov(int x,int y,int op)    {
-    bool jud=0;
+    bool j=0;
     switch(op)  {
-        case 1: {j=(x>0?1:0);break;}            //上
-        case 2: {j=(x<n-1?1:0);break;}          //下
-        case 3: {j=(y>0?1:0);break;}            //左
-        case 4: {j=(y<m-1?1:0);break;}          //右
-        case 5: {j=((y>0&&x>0)?1:0);break;}     //左上
-        case 6: {j=((y>0&&x<n-1)?1:0);break;}   //左下
-        case 7: {j=((y<m-1&&x>0)?1:0);break;}   //右上
-        case 8: {j=((y<m-1&&x<n-1)?1:0);break;} //右下
+        case 1: {
+            j=(x>0?1:0);
+            if (j)  vm=make_pair(-1,0);
+            break;}            //上
+        case 2: {
+            j=(x<n-1?1:0);
+            if (j)  vm=make_pair(1,0);
+            break;}          //下
+        case 3: {
+            j=(y>0?1:0);
+            if (j)  vm=make_pair(0,-1);
+            break;}            //左
+        case 4: {
+            j=(y<m-1?1:0);
+            if (j)  vm=make_pair(0,1);
+            break;}          //右
+        case 5: {
+            j=((y>0&&x>0)?1:0);
+            if (j)  vm=make_pair(-1,-1);
+            break;}     //左上
+        case 6: {
+            j=((y>0&&x<n-1)?1:0);
+            if (j)  vm=make_pair(1,-1);
+            break;}   //左下
+        case 7: {
+            j=((y<m-1&&x>0)?1:0);
+            if (j)  vm=make_pair(-1,1);
+            break;}   //右上
+        case 8: {
+            j=((y<m-1&&x<n-1)?1:0);
+            if (j)  vm=make_pair(1,1);
+            break;} //右下
         default: break;
     }
     return j;
